96d032f418ca053aa19889b45b790e72acb0ee01
Lap1n <philippewlelievre@gmail.com>
2019-02-17 16:18:33 -0500

    updated main with best setup

--- tp1/src/question1/main.py
+++ tp1/src/question1/main.py
@@ -5,7 +5,7 @@ from question1.nn import NN
 
 
 def main():
-    nn = NN()
+    nn = NN(learning_rate=0.1, hidden_dims=(1024, 512), mini_batch_size=100)
     nn.initialize_weights("glorot")
 
     dataset = get_dataset()
