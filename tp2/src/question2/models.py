4818bc6d544747443b3f9e1feb665c1fbd87809c
jgsimard <jeangab23@gmail.com>
2019-03-23 18:18:39 -0400

    dropout modified

--- tp2/src/question2/models.py
+++ tp2/src/question2/models.py
@@ -160,15 +160,15 @@ class RNN(nn.Module):  # Implement a stacked vanilla RNN with Tanh nonlinearitie
         return samples
 
 class GRUCell(nn.Module):
-    def __init__(self, input_size, output_size, bias=True):
+    def __init__(self, input_size, output_size, bias=True, dp_keep_prob=0.5):
         super(GRUCell, self).__init__()
         self.input_size = input_size
         self.output_size = output_size
-        self.input2hidden = nn.Linear(input_size, output_size * 3, bias) #all 3 W in one matrix
-        self.hidden2hidden_gates = nn.Linear(output_size, output_size * 2, bias) # all 2 U in one matrix
-        self.hidden2hidden_cell = nn.Linear(output_size, output_size, bias)
-        # self.cell_state = torch.zeros(output_size)
-
+        self.input2hidden = nn.Linear(input_size, output_size * 3, bias) # W_r, W_z, W_h n one matrix
+        self.hidden2hidden_gates = nn.Linear(output_size, output_size * 2, bias) # U_r, U_z in one matrix
+        self.hidden2hidden_cell = nn.Linear(output_size, output_size, bias) # U_h
+        self.dp_keep_prob = dp_keep_prob
+        self.dropout = nn.Dropout(1-dp_keep_prob)
         self.init_weights_uniform()
 
     def init_weights_uniform(self):
@@ -177,22 +177,17 @@ class GRUCell(nn.Module):
             weights.data.uniform_(-bound, bound)
 
     def forward(self, x, hidden):
-
         w_z, w_r, w_c = self.input2hidden(x).chunk(3, 1)
         u_z, u_r = self.hidden2hidden_gates(hidden).chunk(2, 1)
-
 
         z = torch.sigmoid(u_z + w_z) #update gate
         r = torch.sigmoid(u_r + w_r) #reset gate
 
-        u_c = self.hidden2hidden_cell(hidden*r)
+        u_c = self.hidden2hidden_cell(r*hidden)
 
-        c_tile = torch.tanh(u_c + w_c)
-        c = (1-z)*hidden + z * c_tile
-
-        h = c
-
-        return h
+        h_tilde = torch.tanh(u_c + w_c)
+        h = (1-z)*hidden + z * h_tilde
+        return self.dropout(h)
 
 # Problem 2
 class GRU(nn.Module):  # Implement a stacked GRU RNN
@@ -239,8 +234,9 @@ class GRU(nn.Module):  # Implement a stacked GRU RNN
         self.gru_cells = nn.ModuleList()
         self.gru_cells.append(GRUCell(emb_size,hidden_size))
         for _ in range(num_layers-1):
-            self.gru_cells.append(GRUCell(hidden_size, hidden_size))
-        self.dropout = nn.Dropout(1-dp_keep_prob)
+            self.gru_cells.append(GRUCell(hidden_size, hidden_size, bias=True, dp_keep_prob=dp_keep_prob))
+        self.dropout_embedding = nn.Dropout(1-dp_keep_prob)
+        self.dropout_out = nn.Dropout(1-dp_keep_prob)
         self.linear_out = nn.Linear(hidden_size, vocab_size)
         self.softmax_out = nn.Softmax()
 
@@ -304,10 +300,10 @@ class GRU(nn.Module):  # Implement a stacked GRU RNN
         embeddings = self.embedding(inputs)
         for time_step in range(self.seq_len):
             embedding = embeddings[time_step]
-            new_hidden = [self.gru_cells[0](embedding, hidden[0]).clone()]
+            new_hidden = [self.gru_cells[0](self.dropout_embedding(embedding), hidden[0]).clone()]
             for gru_cell_index in range(1, self.num_layers):
-                new_hidden.append(self.gru_cells[gru_cell_index](self.dropout(new_hidden[-1]).clone(), hidden[gru_cell_index].clone()))
-            logits.append(self.linear_out(self.dropout(new_hidden[-1].clone())).clone())
+                new_hidden.append(self.gru_cells[gru_cell_index](new_hidden[-1].clone(), hidden[gru_cell_index].clone()))
+            logits.append(self.linear_out(self.dropout_out(new_hidden[-1].clone())).clone())
             hidden = torch.stack(new_hidden)
         logits=torch.stack(logits)
         return logits.view(self.seq_len, self.batch_size, self.vocab_size), hidden
@@ -340,10 +336,10 @@ class GRU(nn.Module):  # Implement a stacked GRU RNN
         current_input = input
         for time_step in range(generated_seq_len):
             embedding = self.embedding(current_input)
-            new_hidden = [self.gru_cells[0](self.dropout(embedding), hidden[0]).clone()]
+            new_hidden = [self.gru_cells[0](self.dropout_embedding(embedding), hidden[0]).clone()]
             for gru_cell_index in range(1, self.num_layers):
-                new_hidden.append(self.gru_cells[gru_cell_index](self.dropout(new_hidden[-1]), hidden[gru_cell_index]).clone())
-            logits = self.softmax_out(self.linear_out(self.dropout(new_hidden[-1]))).clone()
+                new_hidden.append(self.gru_cells[gru_cell_index](new_hidden[-1], hidden[gru_cell_index]).clone())
+            logits = self.softmax_out(self.linear_out(self.dropout_out(new_hidden[-1]))).clone()
             current_input = torch.max(logits, 1)
             samples.append(current_input.clone())
             hidden = torch.stack(new_hidden)
