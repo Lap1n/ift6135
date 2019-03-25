e0fb07fd29b4f872058f1d0be3c5417016db2f79
Lap1n <philippewlelievre@gmail.com>
2019-03-25 01:11:45 -0400

    add average loss (there may be errors)

---
+++ tp2/src/average_loss_5_1.py
@@ -0,0 +1,140 @@
+import argparse
+import os
+import sys
+
+import torch
+from torch.autograd import Variable
+
+from tp2.src.models import Batch, RNN, GRU
+import numpy as np
+
+from tp2.src.utils import ptb_raw_data, ptb_iterator, repackage_hidden
+
+parser = argparse.ArgumentParser(description='PyTorch Penn Treebank Language Modeling')
+
+# Arguments you may need to set to run different experiments in 4.1 & 4.2.
+parser.add_argument('--data', type=str, default='data',
+                    help='location of the data corpus')
+parser.add_argument('--model', type=str, default='GRU',
+                    help='type of recurrent net (RNN, GRU, TRANSFORMER)')
+parser.add_argument('--optimizer', type=str, default='SGD_LR_SCHEDULE',
+                    help='optimization algo to use; SGD, SGD_LR_SCHEDULE, ADAM')
+parser.add_argument('--seq_len', type=int, default=35,
+                    help='number of timesteps over which BPTT is performed')
+parser.add_argument('--batch_size', type=int, default=20,
+                    help='size of one minibatch')
+parser.add_argument('--initial_lr', type=float, default=20.0,
+                    help='initial learning rate')
+parser.add_argument('--/', type=int, default=200,
+                    help='size of hidden layers. IMPORTANT: for the transformer\
+                    this must be a multiple of 16.')
+parser.add_argument('--save_best', action='store_true',
+                    help='save the model for the best validation performance')
+parser.add_argument('--num_layers', type=int, default=2,
+                    help='number of hidden layers in RNN/GRU, or number of transformer blocks in TRANSFORMER')
+
+# Other hyperparameters you may want to tune in your exploration
+parser.add_argument('--emb_size', type=int, default=200,
+                    help='size of word embeddings')
+parser.add_argument('--hidden_size', type=int, default=200,
+                    help='size of hidden layers. IMPORTANT: for the transformer')
+parser.add_argument('--dp_keep_prob', type=float, default=0.35,
+                    help='dropout *keep* probability. drop_prob = 1-dp_keep_prob \
+                    (dp_keep_prob=1 means no dropout)')
+
+parser.add_argument('--model_path',
+                    help="model path")
+
+parser.add_argument('--output_length', default='same',
+                    help="Possible values : 'same' or  'double'")
+
+# DO NOT CHANGE THIS (setting the random seed makes experiments deterministic,
+# which helps for reproducibility)
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+
+args = parser.parse_args()
+argsdict = args.__dict__
+argsdict['code_file'] = sys.argv[0]
+# Set the random seed manually for reproducibility.
+torch.manual_seed(args.seed)
+
+# Use the GPU if you have one
+if torch.cuda.is_available():
+    print("Using the GPU")
+    device = torch.device("cuda")
+else:
+    print("WARNING: You are about to run on cpu, and this will likely run out \
+      of memory. \n You can try setting batch_size=1 to reduce memory usage")
+    device = torch.device("cpu")
+
+# LOAD DATA
+print('Loading data from ' + args.data)
+raw_data = ptb_raw_data(data_path=args.data)
+train_data, valid_data, test_data, word_to_id, id_2_word = raw_data
+vocab_size = len(word_to_id)
+print('  vocabulary size: {}'.format(vocab_size))
+
+if args.model == 'RNN':
+    model = RNN(emb_size=args.emb_size, hidden_size=args.hidden_size,
+                seq_len=args.seq_len, batch_size=args.batch_size,
+                vocab_size=vocab_size, num_layers=args.num_layers,
+                dp_keep_prob=args.dp_keep_prob)
+elif args.model == 'GRU':
+    model = GRU(emb_size=args.emb_size, hidden_size=args.hidden_size,
+                seq_len=args.seq_len, batch_size=args.batch_size,
+                vocab_size=vocab_size, num_layers=args.num_layers,
+                dp_keep_prob=args.dp_keep_prob)
+else:
+    print("Model type not recognized.")
+
+model = model.to(device)
+
+# LOSS FUNCTION
+loss_fn = torch.nn.CrossEntropyLoss()
+if args.optimizer == 'ADAM':
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.initial_lr)
+
+
+def run_epoch(model, data):
+    """
+    One epoch of training/validation (depending on flag is_train).
+    """
+    if args.model != 'TRANSFORMER':
+        hidden = model.init_hidden()
+        hidden = hidden.to(device)
+
+    iters = 0
+
+    loss_per_step = torch.zeros(model.seq_len)
+    # LOOP THROUGH MINIBATCHES
+    for step, (x, y) in enumerate(ptb_iterator(data, model.batch_size, model.seq_len)):
+        if args.model == 'TRANSFORMER':
+            batch = Batch(torch.from_numpy(x).long().to(device))
+            model.zero_grad()
+            outputs = model.forward(batch.data, batch.mask).transpose(1, 0)
+            # print ("outputs.shape", outputs.shape)
+        else:
+            inputs = torch.from_numpy(x.astype(np.int64)).transpose(0, 1).contiguous().to(device)  # .cuda()
+            model.zero_grad()
+            hidden = repackage_hidden(hidden)
+            outputs, hidden = model(inputs, hidden)
+
+        targets = torch.from_numpy(y.astype(np.int64)).transpose(0, 1).contiguous().to(device)  # .cuda()
+        # LOSS COMPUTATION
+        # This line currently averages across all the sequences in a mini-batch
+        # and all time-steps of the sequences.
+        # For problem 5.3, you will (instead) need to compute the average loss
+        # at each time-step separately.
+        for step_t in range(model.seq_len):
+            loss_per_step[step_t] += loss_fn(outputs[step_t], targets[step_t]).item()
+
+        iters += 1
+
+    return loss_per_step / float(iters)
+
+
+average_loss_per_step = run_epoch(model, train_data)
+
+with open('./generated_sequence_type_{}_in_length_{}.txt'.format(args.model, args.seq_len), 'a+') as f:
+    print(average_loss_per_step.numpy().tolist(), file=f)
