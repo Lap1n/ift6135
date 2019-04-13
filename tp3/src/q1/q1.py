5a0904a26941c81ecc00b1c5521430185963d113
jgsimard <jeangab23@gmail.com>
2019-04-13 14:57:31 -0400

    jsd done

---
+++ tp3/src/q1/q1.py
@@ -0,0 +1,121 @@
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.optim as optim
+
+
+class BaseModel(nn.Module):
+    def __init__(self, input_size):
+        super(BaseModel, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_size, 32),
+            nn.ReLU(),
+            nn.Linear(32, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class JSDModel(nn.Module):
+    def __init__(self, input_size):
+        super(JSDModel, self).__init__()
+        self.model = nn.Sequential(
+            BaseModel(input_size),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+def compute_jsd(D, x, y):
+    return torch.log(torch.Tensor([2])) + torch.log(D(x)).mean() / 2 + torch.log(1 - D(y)).mean() / 2
+
+
+def JensenShannonDivergence(p, q, n_mini_batch=1000):
+    x = torch.Tensor(next(p))
+    y = torch.Tensor(next(q))
+    input_size = x.size()[1]
+    D = JSDModel(input_size)
+    # optimizer = optim.SGD(D.parameters(), lr=1e-3)
+    optimizer = optim.Adam(D.parameters())
+    for mini_batch in range(n_mini_batch):
+        D.zero_grad()
+        jsd = compute_jsd(D, x, y)
+        jsd.backward(-torch.FloatTensor([1]))
+        optimizer.step()
+    return D, compute_jsd(D, x, y)
+
+
+JSD = JensenShannonDivergence
+
+
+def compute_wd(T, x, y):
+    return T(x).mean() - T(y).mean()
+
+
+def compute_gradient_penality(T, x, y, lambda_grad_penality):
+    a = torch.rand(x.size())
+    a.requires_grad = True
+    z = a * x + (1 - a) * y
+    T_z = T(z)
+    grads = autograd.grad(outputs=T_z, inputs=a, grad_outputs=torch.ones(T_z.size()),
+                          create_graph=True, retain_graph=True, only_inputs=True)[0]
+    gradient_penalty = lambda_grad_penality * ((grads.norm(2, dim=1) - 1) ** 2).mean()
+    return gradient_penalty
+
+
+def WassersteinDistance(p, q, n_mini_batch=1000, lambda_grad_penality=10):
+    x = torch.Tensor(next(p))
+    y = torch.Tensor(next(q))
+    input_size = x.size()[1]
+    T = BaseModel(input_size)
+    optimizer = optim.SGD(T.parameters(), lr=1e-3)
+
+    for mini_batch in range(n_mini_batch):
+        T.zero_grad()
+        wd = compute_wd(T, x, y)
+        total_loss = wd - compute_gradient_penality(T, x, y, lambda_grad_penality)
+        total_loss.backward()
+        optimizer.step()
+    return T, compute_wd(T, x, y)
+
+
+WD = WassersteinDistance
+
+if __name__ == "__main__":
+    from tp3.src.given_code.samplers import distribution1
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    batch_size = 512
+    n_mini_batch = 100
+    p = iter(distribution1(0, batch_size))
+    q = iter(distribution1(1, batch_size))
+    D, jsd = JSD(p, q, n_mini_batch=n_mini_batch)
+    T, wd = WD(p, q, n_mini_batch=n_mini_batch, lambda_grad_penality=0)
+    print(jsd, wd)
+    ##
+    batch_size = 512
+    n_mini_batch = 100
+    computed_jsd = []
+    computed_wd = []
+    phis = np.linspace(-1, 1, 21)
+    # phis = np.linspace(-0.2, 0.2, 5)
+    for phi in phis:
+        p = iter(distribution1(0))
+        q = iter(distribution1(phi))
+        D, jsd = JSD(p, q, n_mini_batch)
+        computed_jsd.append(jsd)
+        T, wd = WD(p, q, n_mini_batch)
+        computed_wd.append(wd)
+
+    plt.plot(phis, computed_jsd, label="JSD")
+    plt.plot(phis, computed_wd, label="WD")
+    plt.legend()
+    plt.show()
